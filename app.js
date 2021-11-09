a826836b7eeab250714e456000fdd85dbf227a44
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-11-08 22:19:40 -0800

    Builds functions to generate random product options

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 'use strict'
-let optionTotal = 3; 
+
+let optionTotal = 3;
 // let optionTotal = parseInt(prompt("how many options?"));
 
-let imgPath = [
+let imgNames = [
+  "boots",
+  "breakfast",
+  "bubblegum",
+  "chair",
+  "cthulhu",
+  "dog-duck",
+  "dragon",
+  "pen",
+  "pet-sweep",
+  "scissors",
+  "shark",
+  "sweep",
+  "tauntaun",
+  "unicorn",
+  "water-can",
+  "wine-glass"
+];
+let imgPaths = [
   "img/boots.jpg",
   "img/breakfast.jpg",
   "img/bubblegum.jpg",
@@ -22,18 +41,53 @@ let imgPath = [
 ];
 
 
+function ProductOption(name, imgPaths) {
+  this.name = name;
+  this.imgPaths = imgPaths;
+  this.displayCounter = 0;
+  ProductOption.all.push(this); // ADDS THIS OBJECT TO ARRAY OF PRODUCTS
+}
+
+ProductOption.all = []; 
+
+
+
+function randomInRange(min, max) {
+  return Math.floor(Math.random(max - min) + min);
+}
+function randomIndex(arrLength){
+  // generates random index from array.length
+  return randomInRange(0, arrLength)
+}
+// function randomProduct() {
+//   // return array.pop(randomIndex)
+// }
+
+function renderImage(id, imgPaths) {
+  let productElem = document.getElementById(id);
+  productElem.setAttribute('src', imgPaths);
+}
 
 function renderProdOptions() {
-  for (let i = 1; i <= optionTotal; i++) {
+  let optionGenArr = [];
+  debugger
+  // creates copy of imgName[] values so that original remains unchanged
+  imgNames.forEach(()=>optionGenArr.push()); 
+
+  for (let i = 1; i <= optionTotal; i++) { // generates one option per pass
     let id = `product-${i}`;
-    // let imgPath = productPathsArray[randIndex()];
-    renderImage(id, imgPath[i]); // imgPath[i] is just for testing
+    let newProdOption = productPathsArray.pop(randomIndex(optionGenArr.length));
+    // 
+    renderImage(id, newProdOption); // imgPaths[i] is just for testing
   }
 }
 
-function renderImage(id, imgPath) {
-  let productElem = document.getElementById(id);
-  productElem.setAttribute('src', imgPath);
+function compileProductOptions(namesArray, pathsArray) { // BUILDS PRODUCT OBJECTS 
+  if (namesArray.length === pathsArray.length) {
+    for (let i = 0; i < pathsArray.length; i++) {
+      new ProductOption(namesArray[i], pathsArray[i]);
+    }
+  } else console.error("name and path arrays are different lengths")
 }
 
-// renderProdOptions();
+compileProductOptions(imgNames, imgPaths);
