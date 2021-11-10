8520956fd49293a7f93cb29dc2de9df447ae20b4
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-11-09 19:02:39 -0800

    Renames several functions and variables for greater consistency

--- app.js
+++ app.js
@@ -39,20 +39,20 @@ let imgPathsArr = [
   "img/wine-glass.jpg"
 ];
 
-function ProductOption(name, imgPath) {
+function Product(name, imgPath) {
   this.name = name;
   this.imgPath = imgPath;
   this.displayCounter = 0;
   this.clickCounter = 0;
-  ProductOption.all.push(this); // ADDS THIS INSTANCE TO ARRAY OF PRODUCTS
+  Product.all.push(this); // ADDS THIS INSTANCE TO ARRAY OF PRODUCTS
 }
 
-ProductOption.all = [];
-ProductOption.option1 = null;
-ProductOption.option2 = null; // TODO: make render a method of ProductOption to gain access to these
-ProductOption.option3 = null;
+Product.all = [];
+Product.option1 = null;
+Product.option2 = null; // TODO: make render a method of Product to gain access to these
+Product.option3 = null;
 
-ProductOption.prototype.renderImage = function (id) {
+Product.prototype.renderImage = function (id) {
   let productElem = document.getElementById(id);
   productElem.src = this.imgPath;
   productElem.alt = this.name;
@@ -65,35 +65,22 @@ function randomIndex(arrLength) { // generates random index from array.length
 }
 
 
-function renderProdOptions() { // TODO: make this function a method of Object, use this.all[] instead of imgNamesArr
-
-  let optionGenArr = [...ProductOption.all]; // creates copy of imgName[] values so that original remains unchanged
-
-  for (let i = 1; i <= optionTotal; i++) { // generates one option per pass
-
-    let id = `product-${i}`;
-
-    let newOptionIndex = randomIndex(optionGenArr.length);
-
-    let newOption = optionGenArr[newOptionIndex];
-    // let thisInstance = ProductOption.all[newOptionIndex];
-    renderImage(id, newOption.imgPath, newOption.name); // RENDERS INDIVIDUAL IMAGE
-    newOption.displayCounter++;
-    optionGenArr.splice(newOptionIndex, 1); // REMOVES THIS ITEM FROM TEMP ARRAY TO PRECLUDE REPEATS when it loops back
-  }
+function renderOptions() {
+  Product.option1.renderImage()  // NEXT 
+  
 }
 
 function instantiateProducts(namesArray, pathsArray) { // BUILDS PRODUCT instances (which get pushed to array )
   if (namesArray.length === pathsArray.length) {
     for (let i = 0; i < pathsArray.length; i++) {
-      new ProductOption(namesArray[i], pathsArray[i]);
+      new Product(namesArray[i], pathsArray[i]);
     }
   } else console.error("name and path arrays are different lengths")
 }
 
 attachEventListeners();
 instantiateProducts(imgNamesArr, imgPathsArr);
-renderProdOptions();
+renderOptions();
 
 function handleClick(event) {
 
@@ -102,7 +89,7 @@ function handleClick(event) {
   if (event.target.classList.value === "product") {
     console.log(event.target.alt);
     // register click count for product and totalclicks
-    renderProdOptions();
+    renderOptions();
   } else console.log("That isn't an option")
 }
 
@@ -113,3 +100,22 @@ function attachEventListeners() {
 
 // associate id with product instance inside render function
 // use id from click event to refer to product instance clickCounter
+
+
+
+
+
+
+// from previous renderOptions()
+// for (let i = 1; i <= optionTotal; i++) { // generates one option per pass
+
+// let id = `option-${i}`;
+
+// let newOptionIndex = randomIndex(optionGenArr.length);
+
+// let newOption = optionGenArr[newOptionIndex];
+// // let thisInstance = Product.all[newOptionIndex];
+// renderImage(id, newOption.imgPath, newOption.name); // RENDERS INDIVIDUAL IMAGE
+// newOption.displayCounter++;
+// optionGenArr.splice(newOptionIndex, 1); // REMOVES THIS ITEM FROM TEMP ARRAY TO PRECLUDE REPEATS when it loops back
+// }
