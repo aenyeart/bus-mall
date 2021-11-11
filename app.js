fc7aedf7ff8935e830702de670996409bd65a59d
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-11-10 22:40:59 -0800

    Adds chart.js bar chart

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const optionTotal = 3; // let optionTotal = parseInt(prompt("how many options?"));
-const maxRounds = 25;
+const maxRounds = 10;
 let roundCounter = 0;
-let imgNamesArr = [
+let imgNamesArray = [
   "boots",
   "breakfast",
   "bubblegum",
@@ -21,7 +21,7 @@ let imgNamesArr = [
   "water-can",
   "wine-glass"
 ];
-let imgPathsArr = [
+let imgPathsArray = [
   "img/boots.jpg",
   "img/breakfast.jpg",
   "img/bubblegum.jpg",
@@ -39,7 +39,8 @@ let imgPathsArr = [
   "img/water-can.jpg",
   "img/wine-glass.jpg"
 ];
-let results = [];
+let votes = [];
+let views = [];
 
 function Product(name, imgPath) {
   this.name = name;
@@ -51,14 +52,15 @@ function Product(name, imgPath) {
 
 Product.all = []; // Creates an array at broad scope so that product instances can be pushed to it or accessed from any function in this codebase.
 
-// These placeholders set us up to handle rendering and display/click counters: 
+// These placeholders below set us up to handle rendering and display/click counters: 
 // 1) From renderOptions(), we call the instance method `.renderImage()`, passing the id of the DOM element that the product image is to be rendered to. 
 // 2) SINCE `.renderImage` is a method of the instances themselves, and 
-          // rendering has already happened before the user clicks, and 
-          // the click event object provides the id of the DOM element that is clicked (via `event.target.id`),
-    // THEN we can access the clicked instance's click counter by 
-          // referring back to its prototype method `renderImage`,
-          // and using the clicked id with some IF-THEN logic and comparison operators to decide whether to increment the .clickCounter of Product.option1, Product.option2, or Product.option3.
+// rendering has already happened before the user clicks, and 
+// the click event object provides the id of the DOM element that is clicked (via `event.target.id`),
+// THEN, 
+// 3) we can access the clicked instance's click counter by 
+// referring back to its prototype method `renderImage`,
+// and using the clicked id with some IF-THEN logic and comparison operators to decide whether to increment the .clickCounter of Product.option1, Product.option2, or Product.option3.
 
 Product.option1 = null;
 Product.option2 = null;
@@ -114,20 +116,20 @@ function handleClick(event) {
 
   if (event.target.classList.value === "option") { // "class" is offlimits in JS as a property name. Must use .classList to view token list
     let usrChoiceId = event.target.id; //
-    
+
     switch (usrChoiceId) {
-    case 'option-1':
-      Product.option1.clickCounter++;
-      // console.log(Product.option1.name, " clicks: ", Product.option1.clickCounter);
-      break;
-    case 'option-2':
-      Product.option2.clickCounter++;
-      // console.log(Product.option2.name, " clicks: ", Product.option2.clickCounter);
-      break;
-    case 'option-3':
-      Product.option3.clickCounter++;
-      // console.log(Product.option3.name, " clicks: ", Product.option3.clickCounter);
-      break;
+      case 'option-1':
+        Product.option1.clickCounter++;
+        // console.log(Product.option1.name, " clicks: ", Product.option1.clickCounter);
+        break;
+      case 'option-2':
+        Product.option2.clickCounter++;
+        // console.log(Product.option2.name, " clicks: ", Product.option2.clickCounter);
+        break;
+      case 'option-3':
+        Product.option3.clickCounter++;
+        // console.log(Product.option3.name, " clicks: ", Product.option3.clickCounter);
+        break;
     }
 
     roundCounter++;
@@ -146,21 +148,67 @@ function attachEventListeners() {
   const clickArea = document.querySelector('main');
   clickArea.addEventListener('click', handleClick);
 }
+
 function removeEventListeners() {
   const clickArea = document.querySelector('main');
   clickArea.removeEventListener('click', handleClick);
 }
+
+function renderChart() {
+  const ctx = document.getElementById('results-chart').getContext('2d');
+  const resultsChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: imgNamesArray,
+      datasets: [{
+        label: '# of clicks',
+        data: votes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          // 'rgba(255, 206, 86, 0.2)',
+          // 'rgba(75, 192, 192, 0.2)',
+          // 'rgba(153, 102, 255, 0.2)',
+          // 'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)'
+          // ,
+          // 'rgba(54, 162, 235, 1)',
+          // 'rgba(255, 206, 86, 1)',
+          // 'rgba(75, 192, 192, 1)',
+          // 'rgba(153, 102, 255, 1)',
+          // 'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+
 function renderResults() {
   const resultsList = document.getElementById("results-list");
 
-  // loops through array of instances, generates DOM element with data for each product 
-  for (let i=0; i < Product.all.length; i++) {
+  // loops through array of instances, generates DOM element with data for each product, pushes to votes and views arrays
+  for (let i = 0; i < Product.all.length; i++) {
     let productInstance = Product.all[i];
+
+    views.push(productInstance.displayCounter); // prepares arrays for chart
+    votes.push(productInstance.clickCounter);
+
     const newResultsListItem = document.createElement("li");
     resultsList.appendChild(newResultsListItem);
     newResultsListItem.id = productInstance.name;
     newResultsListItem.textContent = `${productInstance.name} Clicked: ${productInstance.clickCounter} | Displayed: ${productInstance.displayCounter}`
   }
+  renderChart();
 }
 
 function renderResultsButton() {
@@ -173,7 +221,7 @@ function renderResultsButton() {
   })
 }
 attachEventListeners();
-instantiateProducts(imgNamesArr, imgPathsArr);
+instantiateProducts(imgNamesArray, imgPathsArray);
 generateOptions();
 renderOptions();
 
